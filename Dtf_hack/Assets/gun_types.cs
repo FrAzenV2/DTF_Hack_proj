f54069da0a9995ae0e52805196a301ac3f30c18b
FrAzenV2 <54308705+FrAzenV2@users.noreply.github.com>
2019-11-16 12:37:28 +0300

    делаем оружие

---
+++ Dtf_hack/Assets/gun_types.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gun_types : MonoBehaviour
+{
+
+    public GameObject
+
+    void shoot()
+    {
+
+    }
+}
---
+++ Dtf_hack/Assets/movment_test.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movment_test : MonoBehaviour
+{
+    public GameObject bullet;
+
+    private Rigidbody2D rb;
+
+    public float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+        rb.position = (rb.position + Vector2.right * moveX * speed * Time.deltaTime);
+        rb.position = (rb.position + Vector2.up * moveY * speed * Time.deltaTime);
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            shoot();
+        }
+    }
+
+    
+}
